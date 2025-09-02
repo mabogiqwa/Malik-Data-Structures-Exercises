366026741c8497da4013130ad01f9ba277b8a972
mabogiqwa <mabogiqwa1@gmail.com>
2025-09-02 14:55:21 +0200

    new commit

--- matrix-iterative methods.cpp
+++ matrix-iterative methods.cpp
@@ -92,27 +92,32 @@ EigenResult power_method(std::vector<std::vector<double>> &A, std::vector<std::v
 //Precondition: The passed matrix must be square(rows=cols)
 //Postcondition: The method returns the dominant eigenvalue and associated eigenvector
 
+std::ostream& operator <<(std::ostream &os, EigenResult &result);
+//Postconditon: Prints the contents of the EigenResult object
+
+int main()
+{
+    std::vector<std::vector<double>> A = {{2,1},{1,3}};
+
+    std::vector<std::vector<double>> b = {{1},{0}};
+
+    std::vector<std::vector<double>> w = {{4,1,2},{9,0,3},{2,3,7}};
+
+    std::cout << gauss_seidel(A, b, 50);
+
+    //EigenResult r = power_method(A, X, 3);
+    //std::cout << r;
+
+    return 0;
+}
+
 std::ostream& operator <<(std::ostream &os, EigenResult &result) {
     std::cout << "Eigenvalue: " << result.eigenvalue << std::endl;
-    std::cout << "Eigenresult: " << std::endl;
+    std::cout << "Associated eigenvector: " << std::endl;
     std::cout << result.eigenVector;
     std::cout << std::endl;
 
     return os;
-}
-
-int main()
-{
-    std::vector<std::vector<double>> A = {{4,1},{2,3}};
-
-    std::vector<std::vector<double>> X = {{1},{1}};
-
-    std::vector<std::vector<double>> w = {{4,1,2},{9,0,3},{2,3,7}};
-
-    EigenResult r = power_method(A, X, 3);
-    std::cout << r;
-
-    return 0;
 }
 
 EigenResult power_method(std::vector<std::vector<double>> &A, std::vector<std::vector<double>> &X, int iterations) {
